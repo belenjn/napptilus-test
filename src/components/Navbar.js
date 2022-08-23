0f551ff30ee826170437dd85b61d8bfa9f715c9c
Belen JN <belenjarabanavas31@gmail.com>
2022-08-23 12:49:58 +0200

    Navbar created and styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import "../styles/Navbar.css";
+import oompaLoompa from "../assets/logo-umpa-loompa.png"
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <img src={oompaLoompa} alt="oompa-loompa logo"></img>
+      <h4>Oompa Loompa's Crew</h4>
+    </div>
+  );
+};
